ff5b6b6fd52c2c0d794e513fe201e8dc5c23d54b
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-07-05 23:49:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        //code here.
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.length();i++){
+             mpp[s[i]]++;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(mpp[s[i]]>1) return s[i];
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
