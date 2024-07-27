1a399b7b1b91fcb6fc42bfd375753150e9063f4e
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-07-27 15:13:06 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int countMin(string s){
+    //complete the function here
+      int n=s.size();
+        string a=s,b=s;
+        reverse(b.begin(),b.end());
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=i;
+            dp[0][i]=i;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(a[i-1]==b[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
+                
+            }
+        }
+        
+        return dp[n][n]/2;     
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
