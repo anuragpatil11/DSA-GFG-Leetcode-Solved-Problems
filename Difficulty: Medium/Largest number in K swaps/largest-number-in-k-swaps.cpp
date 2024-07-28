c8d6762ade354c7c9fec099ad1a34492f8680c02
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-07-28 15:35:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the largest number after k swaps.
+    void solve(string str, int k,int ind,string& maxi) {
+        maxi = max(maxi,str);
+        if (k == 0) return; 
+        int n = str.length();
+        for (int i = ind; i < n-1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (str[i] < str[j]) {
+                    swap(str[i], str[j]);
+                    solve(str, k - 1,i+1,maxi); // Recursive call
+                    swap(str[i], str[j]);
+                    
+                }
+            }
+        }
+    }
+    string findMaximumNum(string str, int k)
+    {
+      string maxi = "";
+       solve(str,k,0,maxi);
+       return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> k >> str;
+        Solution ob;
+        cout<< ob.findMaximumNum(str, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
