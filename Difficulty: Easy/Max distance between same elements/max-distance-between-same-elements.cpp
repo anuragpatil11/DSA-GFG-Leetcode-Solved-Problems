664fe67eb6c1183eca1fac396251b7bc10db67b6
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-05 19:58:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max distance between same elements/max-distance-between-same-elements.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxDistance(vector<int> &arr) {
+        // Code here
+        unordered_map<int, int> pos;
+    int m = 0;
+    for(int i=0; i<arr.size(); i++) {
+        if(pos.find(arr[i]) == pos.end())
+            pos[arr[i]] = i;
+        else
+            m = max(m, i - pos[arr[i]]);
+    }
+    return m;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+
+        stringstream s1(input);
+        int num;
+        while (s1 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.maxDistance(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
