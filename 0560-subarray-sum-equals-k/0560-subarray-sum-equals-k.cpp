f30edd32e2595c9c8f27ae09ce07ab314e9c0e67
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-07 13:32:20 +0530

    Time: 2760 ms (5.82%), Space: 33.3 MB (79.48%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            int sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+                if(sum==k){
+                    cnt++;
+                }
+            }
+        }
+        // int maxi=nums[0];
+        // for(int i=0;i<nums.size();i++){
+        //     sum+=nums[i];
+        //     maxi=max(maxi,sum);
+        //     if(maxi==k){
+        //         cnt++;
+        //     }
+        //     if(sum<0){
+        //         sum=0;
+        //     }
+    // }
+        return cnt;
+    }
+};
