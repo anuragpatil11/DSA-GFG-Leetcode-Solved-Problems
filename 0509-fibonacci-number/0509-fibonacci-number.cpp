b36d386cbefce8986ebff849eb8f0f022bb66398
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-07 22:33:19 +0530

    Time: 3 ms (66.80%), Space: 7.4 MB (16.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0)
-            return 0;
+        if (n == 0 || n == 1) return n;
+        int a = 0, b = 1, c;
+        // int a=0;
+        // int b=1;
+        // int c;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+        // Recurrsion
+//         if (n == 0)
+//             return 0;
 
-        if (n == 1)
-            return 1;
+//         if (n == 1)
+//             return 1;
 
-        int ans = fib(n - 1) + fib(n - 2);
+//         int ans = fib(n - 1) + fib(n - 2);
 
-        return ans; 
+//         return ans; 
     }
 };
