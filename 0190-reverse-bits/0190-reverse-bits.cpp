b61a55baa29383abd2ff7941452a7f8123e07677
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-14 13:46:29 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (11.67%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        string s = bitset<32>(n).to_string();
+        for (int i = 0; i < 32; i++) {
+            if (s[i] == '1')
+                ans += pow(2, i);
+        }
+        return ans;
+    }
+};
