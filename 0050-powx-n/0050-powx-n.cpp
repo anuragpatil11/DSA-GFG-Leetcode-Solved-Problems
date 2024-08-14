3b8fe13ee1e5185ad35c3095095a24179d8dabc3
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-14 15:08:06 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (45.34%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    double pow(double x, int n)
+    {
+        if(n==0)
+        return 1;
+        if(n==1)
+        return x;
+
+        double ans = double(pow(x,n/2));
+        if(n%2 == 0)
+        {
+            double ans2 = double(ans*ans);
+            return ans2;
+        } 
+        else
+        {
+            double ans2 = double(x*ans*ans);
+            return ans2;
+        }
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans1 = pow(x,n);
+        if(n<0)
+        ans1 = 1/ans1;
+        return ans1;
     }
 };
