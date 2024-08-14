816c24bf98e6da941bf5eb0351e87b23c90baad3
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-14 14:23:05 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (62.47%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-       vector<int> ans(n+1);
-        ans[0]=0;
-        for(int i=0;i<=n;i++){
-           
-            ans[i]=ans[i/2]+i%2;
-        }
-        // ans.push_back(sum);   
-    // for (int i = 1; i <= n; ++i) {
-    //     ans[i] = ans[i >> 1] + (i & 1);
-    // }
+       vector<int> ans(n+1);  
+    for (int i = 1; i <= n; ++i) {
+        ans[i] = ans[i >> 1] + (i & 1);
+    }
     return ans;
     }
 };
