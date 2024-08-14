12f36508a422f6ce0e63d69a0f4446de29818231
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-14 13:16:11 +0530

    Time: 776 ms (46.23%), Space: 10.6 MB (67.42%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    bool searchFromPosition(vector<vector<char>>& board, string word, int i, int j, int index) {
-    if (index == word.length()) {
-        return true;
-    }
-    
-    if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index]) {
-        return false;
-    }
-    
-    // To avoid revisiting the same cell, temporarily mark the cell as visited
-    char temp = board[i][j];
-    board[i][j] = '#';
-
-    // Check in all four directions
-    bool found = searchFromPosition(board, word, i + 1, j, index + 1) ||
-                 searchFromPosition(board, word, i - 1, j, index + 1) ||
-                 searchFromPosition(board, word, i, j + 1, index + 1) ||
-                 searchFromPosition(board, word, i, j - 1, index + 1);
-
-    // Restore the cell value
-    board[i][j] = temp;
-
-    return found;
-}
 
 bool exist(vector<vector<char>>& board, string word) {
-    int row = board.size();
-    int col = board[0].size();
-
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            if (searchFromPosition(board, word, i, j, 0)) {
+    int m = board.size();
+        int n = board[0].size();
+        
+        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
+            if (k == word.length()) {
                 return true;
             }
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
+                return false;
+            }
+            
+            char temp = board[i][j];
+            board[i][j] = '\0';
+            
+            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
+                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
+                return true;
+            }
+            
+            board[i][j] = temp; 
+            return false;
+        };
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (backtrack(i, j, 0)) {
+                    return true;
+                }
+            }
         }
-    }
-
-    return false;
+        return false;
 }
 };
