bf0ba3736d3465cdfa8c887ffb7d14989429fce3
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-15 12:23:51 +0530

    Time: 82 ms (72.58%), Space: 96.2 MB (47.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -10,16 +10,6 @@ public:
             
             profit=max(profit,(prices[i]-buyStock));
         }
-//        int buyPrice = prices[0];
-//         int profit = 0;
-
-//         for (int i = 1; i < prices.size(); i++) {
-//             if (buyPrice > prices[i]) {
-//                 buyPrice = prices[i];
-//             }
-
-//             profit = max(profit, prices[i] - buyPrice);
-//         }
 
         return profit;     
     }
