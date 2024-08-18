6f3dc135dd3e0bbc0672656dcc2aec9d983e9c2b
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-18 19:38:08 +0530

    Time: 3 ms (47.39%), Space: 7.6 MB (49.54%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        // Check if the sizes of the strings are different
-    if (s.size() != goal.size()) {
-        return false;
-    }
-    
-    // Append s to itself
-    string temp = s + s;
-    
-    // Check if goal is a substring of temp
-    if (temp.find(goal) != string::npos) {
-        return true;
-    }
-    
-    return false;
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        string concatenated = goal + goal;
+
+        return concatenated.find(s) != string::npos;
     }
 };
