d6dbad64764e861cbf88ba8f0ad28a31c0cd09cd
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-20 11:52:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome/palindrome.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int num=n;
+		    int ans=0;
+		    while(n!=0){
+		        int digit=n%10;
+		        ans=(ans*10)+digit;
+		        n=n/10;
+		    }
+		    if(num==ans){
+		        return "Yes";
+		    }
+		    else{
+		        return "No";
+		    }
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
