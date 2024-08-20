1c814da395447fbf90b45c4cf14083d36c5a5545
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-21 00:34:34 +0530

    Time: 7 ms (79.79%), Space: 10 MB (91.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       vector<bool> v(256,0);
-        int l=0,r=0,maxLen=0;
-        while(r<s.size()){
-            if(v[s[r]]!=1){
-                v[s[r]]=1;
-                 maxLen=max(maxLen,r-l+1);
-                r++;
-            }
-            else{
-            
-            while(v[s[r]]>=1){
-                v[s[l]]=0;
-                l++;
-            }
-            }
-           
+       vector<bool> v(256, false);  // To store the occurrence of characters
+    int l = 0, r = 0, maxLen = 0;
+
+    while (r < s.size()) {
+        if (!v[s[r]]) {  // If character at r is not in the window
+            v[s[r]] = true;
+            maxLen = max(maxLen, r - l + 1);  // Update the max length of the substring
+            r++;  // Move the right pointer
+        } else {
+            // If character at r is a duplicate, remove characters from the left
+            v[s[l]] = false;
+            l++;  // Move the left pointer
         }
-            return maxLen;
+    }
+
+    return maxLen;
     }
 };
