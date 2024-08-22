1fa710eb296ed36cdeb91d9bdafaafd7dce1f9f9
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-22 14:41:34 +0530

    Time: 109 ms (56.39%), Space: 97.7 MB (67.49%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-         int n = nums.size();
-        long long count = 0, score = 0, sum = 0;
-        // standard sliding windows
-        for (int right = 0, left = 0; right < n; right++) {
-            // update sum
-            sum += nums[right];
-            // update score
-            score = sum * (right - left + 1);
-            // pruning current window if invalid i.e., window score >= k
-            while (left <= right && score >= k){
-                sum -= nums[left++];
-                score = sum * (right - left + 1);
-            }
-            // update count of all such valid subarray
-            if (score < k) count += right - left + 1;
+        int i = 0, j = 0, n = nums.size();
+        long long ans = 0, sum = 0;
+        while(j<n)
+        {
+            sum += nums[j];
+            while(sum*(j-i+1) >= k) sum -= nums[i++];
+            ans += j-i+1;
+            j++;
         }
-		return count;
+        return ans;
     }
 };
