79a66cb36736383ae6a3f1de6be5f4f82188ba9d
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-08-25 15:23:06 +0530

    Time: 7 ms (47.84%), Space: 16.3 MB (60.40%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-         int a = -1, b = -1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                a=i;
-                break;
-            }
+        int i = 0;
+    int j = nums.size() - 1;
+    int a = -1, b = -1;
+
+    // Find the first occurrence of the target
+    while (i <= j) {
+        if (nums[i] == target && a == -1) {
+            a = i;  // Mark the first occurrence
         }
-        for(int j=nums.size()-1;j>=0;j--){
-            if(nums[j]==target){
-                b=j;
-                break;
-            }
+        if (nums[j] == target && b == -1) {
+            b = j;  // Mark the last occurrence
         }
-//         int i = 0;
-//     int j = nums.size() - 1;
-//     int a = -1, b = -1;
 
-//     // Find the first occurrence of the target
-//     while (i <= j) {
-//         if (nums[i] == target && a == -1) {
-//             a = i;  // Mark the first occurrence
-//         }
-//         if (nums[j] == target && b == -1) {
-//             b = j;  // Mark the last occurrence
-//         }
+        // If both a and b are set, break the loop
+        if (a != -1 && b != -1) {
+            break;
+        }
 
-//         // If both a and b are set, break the loop
-//         if (a != -1 && b != -1) {
-//             break;
-//         }
-
-//         // Move the pointers inward
-//         if (a == -1) i++;  // Increment `i` if the first occurrence is not found
-//         if (b == -1) j--;  // Decrement `j` if the last occurrence is not found
-//     }
+        // Move the pointers inward
+        if (a == -1) i++;  // Increment `i` if the first occurrence is not found
+        if (b == -1) j--;  // Decrement `j` if the last occurrence is not found
+    }
 
     return {a, b};
     }
